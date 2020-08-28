f6a282d602da8d194d32cb7aaa3d4c2d3a76f42c
Nguyen Pham <phamvannguyen.haui@gmail.com>
2020-08-28 10:24:34 +0700

    tmp...

--- app/frontend/frontend_config.php
+++ app/frontend/frontend_config.php
@@ -1,7 +1,8 @@
 <?php
 return array(
     'router' => array(
-        'tin-tuc' => 'blogController/index',
+		//'' => 'blog/index',
+        'tin-tuc' => 'blog/index',
         'tin-tuc/(slug:any)' => 'blog/category',        //controller/action
         'tin-tuc/(slug:any)/(post_slug:any)-p(id:number)' => 'blog/view',
         'tin-tuc/(post_slug:any)-p(id:number)' => 'blog/view',
